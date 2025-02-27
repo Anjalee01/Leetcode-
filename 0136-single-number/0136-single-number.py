c96fe05d3c6b39e506e70f1b002c3aa7b4a18588
Anjalee01 <93911402+Anjalee01@users.noreply.github.com>
2025-02-27 10:25:39 +0500

    Time: 8 ms (24.45%), Space: 19.9 MB (14.52%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,25 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
-        if len(nums) == 1:
-            return nums[0]
+        # if len(nums) == 1:
+        #     return nums[0]
 
-        newset = set(nums)  #[2,1]
+        # newset = set(nums)  #[2,1]
 
-        for i in newset:
-            if nums.count(i) == 1:
+        # for i in newset:
+        #     if nums.count(i) == 1:
+        #         return i
+
+
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+        
+        for i in dic:
+            if dic[i] == 1:
                 return i
 
 
