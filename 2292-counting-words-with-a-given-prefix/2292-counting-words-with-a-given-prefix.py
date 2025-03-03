2e1f375adfd1c7aee1ff96e0b4f94d1b9faf2f7b
Anjalee01 <93911402+Anjalee01@users.noreply.github.com>
2025-03-03 12:44:19 +0500

    Time: 0 ms (100%), Space: 17.7 MB (90.53%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
@@ -1,7 +1,11 @@
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
-        ans=0
-        for i in words: #O(n)
-            if pref== i[0:len(pref)]:# slicing has complexity of O(n) but n is already assigned so we consider O(m) where m is length of pref
-                ans+=1
-        return ans
+
+        count = 0
+
+        for word in words:
+            if word.startswith(pref):
+                count += 1
+
+        return count
+        
